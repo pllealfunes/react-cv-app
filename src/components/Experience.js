44e207e49a78647cd92794cc3fb1d4b7e65f3ce5
Paula Leal <paula61395@gmail.com>
2022-07-28 13:53:09 -0400

    Created form for each section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export default function Experience() {
+    const [exFormData, setExFormData] = React.useState({
+        companyName: "",
+        title: "",
+        description: "",
+        date: ""
+    });
+
+    console.log(exFormData);
+
+    function handleChange(event) {
+        setExFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        });
+    }
+
     return (
-        <div>Experience</div>
+        <div className='exinfo-container'>
+            <h1>General Information</h1>
+            <form className='exInfoForm'>
+                <input
+                    type='text'
+                    name='companyName'
+                    placeholder='Company Name'
+                    onChange={handleChange}
+                    value={exFormData.companyName}
+                />
+                <input
+                    type='text'
+                    name='title'
+                    placeholder='Position Title'
+                    onChange={handleChange}
+                    value={exFormData.title}
+                />
+                <input
+                    type='text'
+                    name='date'
+                    placeholder='Time of Work'
+                    onChange={handleChange}
+                    value={exFormData.date}
+                />
+                <textarea
+                    name="description"
+                    placeholder='Description'
+                    onChange={handleChange}
+                    value={exFormData.comments}
+                />
+            </form>
+        </div>
     );
 }
--- src/components/General.js
+++ src/components/General.js
@@ -2,7 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 export default function General() {
+
+    const [genFormData, setGenFormData] = React.useState({
+        fullName: "",
+        email: "",
+        phone: ""
+    });
+
+    console.log(genFormData);
+
+    function handleChange(event) {
+        setGenFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value
+            }
+        });
+    }
+
     return (
-        <div>General</div>
+        <div className='geninfo-container'>
+            <h1>General Information</h1>
+            <form className='genInfoForm'>
+                <input
+                    type='text'
+                    name='fullName'
+                    placeholder='Full Name'
+                    onChange={handleChange}
+                    value={genFormData.fullName}
+                />
+                <input
+                    type='email'
+                    name='email'
+                    placeholder='Email'
+                    onChange={handleChange}
+                    value={genFormData.email}
+                />
+                <input
+                    type='tel'
+                    name='phone'
+                    placeholder='Phone Number'
+                    onChange={handleChange}
+                    value={genFormData.phone}
+                />
+            </form>
+        </div>
     );
 }
